d020729cbbb0eddce6b6ec47479fbd7a20f3e843
BadAimWeeb <badaimweeb@protonmail.com>
2023-09-19 20:44:54 -0500

    full api

--- src/index.ts
+++ src/index.ts
@@ -4,39 +4,106 @@ import { existsSync } from "fs";
 import path from "path";
 
 import { Server as Pv2dServer, keyGeneration } from "@badaimweeb/js-protov2d";
-import { DTSocketServer, InitProcedureGenerator } from "@badaimweeb/js-dtsocket";
+import { DTSocketServer, InitProcedureGenerator, type Socket } from "@badaimweeb/js-dtsocket";
 import z from "zod";
 
-const p = InitProcedureGenerator<
-    {}, {
-        account?: string
-    }
->();
+type SpecificData = never;
+type SpecificDataResponse = never;
+
+type GlobalData = {
+    [accountID: string]: [tabID: string, expires: number][]
+};
+
+type LocalData = {
+    account?: string,
+    outputAccount?: string
+}
+
+const gState: GlobalData = {};
+
+const p = InitProcedureGenerator<GlobalData, LocalData>();
 
 const apiServer = new DTSocketServer<
-    {}, {
-        account?: string
-    }, {
+    GlobalData,
+    LocalData,
+    {
         csEvents: {
             data: (tabID: string, data: string) => void; // send fb data to relay
+            specificData: (nonce: number, specificData: SpecificDataResponse) => void; // browser response to requestSpecificData
         },
         scEvents: {
             recData: (tabID: string, data: string) => void; // data sent from browser to relay server
             injData: (data: string, tabID?: string | undefined) => void; // data sent from fca to relay server
+            newTab: (tabID: string[]) => void; // new tab created
+            delTab: (tabID: string[]) => void; // tab closed
+            requestSpecificData: (tabID: string, specificData: SpecificData, nonce: number) => void; // request specific data from browser
         }
     }
 >({
+    // Browser-side
     registerInput: p
         .input(z.string())
         .resolve(async (_gState, lState, input) => {
             lState.account = input;
             return true;
         }),
+    registerInputTab: p
+        .input(
+            z.string()
+                .or(z.array(z.string()))
+        )
+        .resolve(async (_gState, lState, input, socket) => {
+            if (lState.account === void 0) return false;
+            if (!Array.isArray(input)) input = [input];
+
+            for (const tabID of input) {
+                if (_gState[lState.account] === void 0) _gState[lState.account] = [];
+                let index = _gState[lState.account].findIndex((v) => v[0] === tabID)
+                if (index + 1) {
+                    _gState[lState.account][index][1] = Date.now() + 1000 * 60; // 60s to live
+                }
+            }
+
+            return true;
+        }),
+    unregisterInputTab: p
+        .input(
+            z.string()
+                .or(z.array(z.string()))
+        )
+        .resolve(async (_gState, lState, input, socket) => {
+            if (lState.account === void 0) return false;
+            if (!Array.isArray(input)) input = [input];
+
+            for (const tabID of input) {
+                if (_gState[lState.account] === void 0) _gState[lState.account] = [];
+                let index = _gState[lState.account].findIndex((v) => v[0] === tabID)
+                if (index + 1) {
+                    _gState[lState.account].splice(index, 1);
+                }
+            }
+
+            return true;
+        }),
+
+    // FCA-side
     registerOutput: p
         .input(z.string())
         .resolve(async (_gState, lState, input, socket) => {
             socket.rooms.clear();
             socket.join(input);
+
+            lState.outputAccount = input;
+        }),
+    getTabs: p
+        .input(z.void())
+        .resolve(async (_gState, lState) => {
+            if (lState.outputAccount === void 0) return [];
+            if (_gState[lState.outputAccount] === void 0) return [];
+
+            return _gState[lState.outputAccount]
+                .filter((v) => v[1] > Date.now())
+                .map((v) => v[0]);
         }),
     injectData: p
         .input(z.object({
@@ -44,9 +111,26 @@ const apiServer = new DTSocketServer<
             tabID: z.string().optional()
         }))
         .resolve(async (_gState, lState, input, socket) => {
-            // todo
+            if (lState.outputAccount === void 0) return false;
+            if (_gState[lState.outputAccount] === void 0) return false;
+            _gState[lState.outputAccount] = _gState[lState.outputAccount]
+                .filter((v) => v[1] > Date.now() ? true : (socket.to(v[0]).emit("delTab", [v[0]]), false));
+
+            if (input.tabID === void 0) {
+                // Select random tab
+                const tabs = _gState[lState.outputAccount];
+                if (tabs.length === 0) return false;
+
+                const tab = tabs[Math.floor(Math.random() * tabs.length)];
+                apiServer.to(lState.outputAccount).emit("injData", input.data, tab[0]);
+            } else {
+                if (_gState[lState.outputAccount].findIndex((v) => v[0] === input.tabID) === -1) return false;
+                apiServer.to(lState.outputAccount).emit("injData", input.data, input.tabID);
+            }
+
+            return true;
         })
-});
+}, gState);
 
 let key: {
     privateKey: string;
